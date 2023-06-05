b324e50fe89e78d9b75817250a3f573884efbe69
Joanna Seybold <126947675+clever-girl31@users.noreply.github.com>
2023-06-04 22:38:44 -0500

    create blog post client side done

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,35 @@
 console.log('dashboard.js is linked')
+
+const newBlogTitle = document.getElementById('newBlogTitle')
+const newBlogText = document.getElementById('newBlogText')
+const blogSubmit = document .getElementById('blogSubmit')
+
+const submitBlogHandler = async (event) => {
+  event.preventDefault();
+  console.log('click')
+  const title = newBlogTitle.value
+  console.log(title)
+  const text = newBlogText.value
+  
+  if (title && text) {
+    console.log('content')
+    const response = await fetch('/api/blogs', {
+      method: 'POST',
+      body: JSON.stringify({ title, text }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    if (response.ok) {
+      window.alert('Blog Posted!')
+      // setTimeout(() => {
+      //   location.reload();
+      // }, 3000);
+      location.reload()
+
+    } else {
+      console.log('no content')
+    }
+  }
+}
+
+blogSubmit.addEventListener('click', submitBlogHandler)
